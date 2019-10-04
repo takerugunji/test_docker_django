125086594939ed91ef46e2aa49012022caaab8f6
gunji takeru <gunjitakeru@MacBook-Air.local>
2019-10-04 17:57:04 +0900

    first commit

---
+++ myblogapp/posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    #return HttpResponse("Hello World! このページは投稿のインデックスです。")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'posts/about.html')
+
+def post_detail(request,post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+# Create your views here.
